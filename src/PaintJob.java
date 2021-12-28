cad49aef5a8690eb32cba07890d327b8d80c4e24
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2021-12-28 10:26:41 -0300

    Initial commit

---
+++ src/PaintJob.java
@@ -0,0 +1,28 @@
+public class PaintJob {
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+        double buckets = Math.ceil(((width * height) / areaPerBucket) - extraBuckets);
+        return (int) buckets;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+        double buckets = Math.ceil((width * height) / areaPerBucket);
+        return (int) buckets;
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+        if(area <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+        double buckets = Math.ceil(area / areaPerBucket);
+        return (int) buckets;
+    }
+}
